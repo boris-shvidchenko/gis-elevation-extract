0814e76a6dc72cf2cfea7ac4249635f355c5e352
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2023-08-11 14:10:30 -0700

    Update elevation_extract.py
    Additional geoprocessing tools added via arcpy, code cleaned up as needed

--- ElevationExtract/elevation_extract.py
+++ ElevationExtract/elevation_extract.py
@@ -6,7 +6,7 @@ outputDB = arcpy.GetParameterAsText(1)
 elevationStart = arcpy.GetParameterAsText(2)
 elevationEnd = arcpy.GetParameterAsText(3)
 clipExtent = arcpy.GetParameterAsText(4)
-deleteFiles = arcpy.GetParameterAsText(5)
+deleteFiles = arcpy.GetParameter(5)
 
 # Formatted elevation variables
 elevStart = int(round(float(elevationStart)))
@@ -14,12 +14,14 @@ elevEnd = int(round(float(elevationEnd)))
 
 # SQL clause
 sql = 'VALUE >= {0} AND VALUE <= {1}'.format(elevationStart, elevationEnd)
- 
+
 # Output feature class names
 elevationExtract = '{0}/Elevation_{1}_to_{2}'.format(outputDB, elevStart, elevEnd)
 elevationExtract_Int = '{0}/Elevation_{1}_to_{2}_int'.format(outputDB, elevStart, elevEnd)
 elevationExtract_Poly = '{0}/Elevation_{1}_to_{2}_poly'.format(outputDB, elevStart, elevEnd)
 elevationExtract_Dis = '{0}/Elevation_{1}_to_{2}_dis'.format(outputDB, elevStart, elevEnd)
+elevationExtract_Clip = '{0}/Elevation_{1}_to_{2}_clip'.format(outputDB, elevStart, elevEnd)
+elevationExtract_Final = '{0}/Elevation_{1}_to_{2}_final'.format(outputDB, elevStart, elevEnd)
 
 # === 1. Extract by Attributes ===
 arcpy.AddMessage('Extracting attributes...')
@@ -41,15 +43,28 @@ arcpy.AddMessage('Polygon exported.')
 # === 4. Dissolve polygon ===
 arcpy.AddMessage('Dissolving polygon...')
 arcpy.management.Dissolve(elevationExtract_Poly, elevationExtract_Dis)
-arcpy.AddMessage('Polygon dissolved')
- 
+arcpy.AddMessage('Polygon dissolved.')
+
 # === 5. Clip polygon (Optional) ===
+if clipExtent is not '':
+    arcpy.AddMessage('Clipping polygon to clip extent...')
+    arcpy.analysis.Clip(elevationExtract_Dis, clipExtent, elevationExtract_Clip)
+    arcpy.AddMessage('Polygon clipped.')
 
-# === 6. Add/calculate an 'Acres' field to final polygon  ===
-
+# === 6. Rename the final feature ===
+arcpy.AddMessage('Renaming feature class...')
+if clipExtent is not '':
+    arcpy.management.Rename(elevationExtract_Clip, elevationExtract_Final)
+else:
+    arcpy.management.Rename(elevationExtract_Dis, elevationExtract_Final)
+arcpy.AddMessage('Feature class renamed.')
+ 
 # === 7. Delete Features (Optional) ===
+if deleteFiles is True:
+    arcpy.AddMessage('Deleting unneeded feature classes...')
+    arcpy.management.Delete([elevationExtract, elevationExtract_Int, elevationExtract_Poly])
+    if clipExtent is not '':
+        arcpy.management.Delete(elevationExtract_Dis)
+    arcpy.AddMessage('Unneeded feature classes deleted.')
 
-# ==============================
-
-# test
-# arcpy.AddMessage()
+# === 8. Add/calculate an 'Acres' field to final polygon  ===
